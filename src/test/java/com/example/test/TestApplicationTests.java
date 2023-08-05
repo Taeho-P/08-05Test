09202d516f3a3d844ae6a280e13885c58ad9fdad
taeho <goekddkdlel@naver.com>
2023-08-05 16:09:16 +0900

    2023-08-05 태호 commit

---
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //안녕하세요 태호입니다.
+    }
+
+}
